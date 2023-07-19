c2fa894ed6cdb73eff51fed0223e4c8667ea57dd
Filippo Chirigoni <filippo.chirigoni@mc-dsi.com>
2023-07-19 09:58:13 +0200

    Add unauthorized error

--- code/libs/Response/Response.php
+++ code/libs/Response/Response.php
@@ -5,14 +5,15 @@ class Response
     const HTTP_CREATED = 201;
     const HTTP_NO_CONTENT = 204;
     const HTTP_BAD_REQUEST = 400;
+    const HTTP_UNAUTHORIZED = 401;
     const HTTP_FORBIDDEN = 403;
     const HTTP_NOT_FOUND = 404;
     const HTTP_I_AM_A_TEAPOT = 418;
     const HTTP_INTERNAL_SERVER_ERROR = 500;
     const HTTP_BAD_GATEWAY = 502;
 
-    const JSON_CONTENT      = 'Content-type: application/json';
-    const TEXT_CONTENT      = 'Content-type: text/plain';
+    const JSON_CONTENT = 'Content-type: application/json';
+    const TEXT_CONTENT = 'Content-type: text/plain';
 
     /**
      * Return the desired HTTP code
